50b1ba0f2a275282bad86e99578fedc93f073b07
tangtao <tt@qihyunxin.com>
2019-08-24 11:08:13 +0800

    优化ServerTCP和日志打印

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"github.com/tgo-team/tgo/src"
 )
 
-func main()  {
+func main() {
 
 	// 创建TGO
 	tg := tgo.New()
 	// 指定server
-	tg.UseServer(tgo.NewServerTCP("0.0.0.0:0",nil,nil))
+	tg.UseServer(tgo.NewServerTCP(tgo.TcpHandshake(func(packet interface{}, conn tgo.StatefulConn) bool {
+		return true
+	}), tgo.TcpAddr("0.0.0.0:0")))
 	// 指定包处理者
 	tg.UseHandler(func(ctx tgo.Context) {
 
